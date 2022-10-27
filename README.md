51eadeb3c20b840e59f33f6c03543426ac6a096b
jahirul <mdjahirul132000@gmail.com>
2022-10-27 16:15:22 +0600

    changes in UrlShortnerServiceProvider class

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Url Shortner Package
 
+[![Issues](https://img.shields.io/github/issues/mazhar-rony/url-shortner-laravel-package.svg?style=flat-square)](https://github.com/mazhar-rony/url-shortner-laravel-package/issues)
+[![Fork](https://img.shields.io/github/forks/mazhar-rony/url-shortner-laravel-package.svg?style=flat-square)](https://github.com/mazhar-rony/url-shortner-laravel-package/network/members)
+[![Stars](https://img.shields.io/github/stars/mazhar-rony/url-shortner-laravel-package.svg?style=flat-square)](https://github.com/mazhar-rony/url-shortner-laravel-package/stargazers)
+
 
 ## This package will let users create short url and save it in database
 
-composer require mazhar-rony/url-shortner
+Installion: composer require mazhar-rony/url-shortner
--- src/UrlShortnerServiceProvider.php
+++ src/UrlShortnerServiceProvider.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Mazhar\UrlShortner;
 
 use Illuminate\Support\ServiceProvider;
@@ -8,13 +9,16 @@ class UrlShortnerServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        
     }
 
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-        $this->loadViewsFrom(__DIR__.'/views', 'url-shortner');
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+        $this->loadViewsFrom(__DIR__ . '/views', 'url-shortner');
+        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+
+        $this->publishes([
+            __DIR__ . '/views' => resource_path('views/vendor/url-shortner'),
+        ]);
     }
 }
